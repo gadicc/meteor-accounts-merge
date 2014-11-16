f7a7b351b2b87f5f560d30b1b2cd3b261daeb89b
Gadi Cohen <dragon@wastelands.net>
2014-11-16 12:08:24 +0200

    meteor 0.9+ support

--- package.js
+++ package.js
@@ -1,4 +1,6 @@
 Package.describe({
+	name: 'gadicohen:accounts-merge',
+	version: '0.0.1',
 	summary: 'Let user login to and add multiple services to one account'
 });
 
@@ -6,11 +8,17 @@ Package.describe({
 Npm.depends({
 	"github": "0.1.12",
 	"googleapis": "0.4.7",
-	"googlemaps": "0.1.9",
 	"fbgraph": "0.2.10"
 });
 
 Package.on_use(function (api) {
+	if (api.versionsFrom) {
+		api.versionsFrom('0.9.4');
+		api.use('meteorhacks:npm@1.2.0', 'server');
+	} else {
+		api.use('npm', 'server');
+	}
+
 	api.use(['accounts-base', 'service-configuration'], 'server');
 	api.use('underscore', 'server');
 	api.use(
@@ -26,7 +34,6 @@ Package.on_use(function (api) {
 
 	api.add_files('accounts-merge.js', 'server');
 
-	api.use('npm', 'server');
 	api.add_files('services.js', 'server');
 	api.export('AccountsExtra', 'server');
 
